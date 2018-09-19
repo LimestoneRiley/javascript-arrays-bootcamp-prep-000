4af8857be6539a4bc0070eb16d04fd7b1f0bc5fd
Robert Riley <rob_a_riley@yahoo.com>
2018-09-19 15:17:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (arrayName, arrayElement) {
-  var newArray = arrayName
-  arrayName = (arrayElement, ...arrayName)
-  return newArray
+  var newArray = arrayName;
+  arrayName = (arrayElement, ...arrayName);
+  return newArray;
 }
 
 
 function destructivelyAddElementToBeginningOfArray (arrayName, arrayElement) {
   arrayName.unshift (arrayElement);
-  return arrayName  
+  return arrayName;
 }
