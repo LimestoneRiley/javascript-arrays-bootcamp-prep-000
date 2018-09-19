5bb4d61e602aed7f1a19efea6f2e9efd9fce9875
Robert Riley <rob_a_riley@yahoo.com>
2018-09-19 17:34:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function addElementToEndOfArray(array, element){
 
 function destructivelyAddElementToEndOfArray(array, element){
   array = array.push [element];
-  return (array);
+  return array;
 }
 
 function accessElementInArray(array, index){
