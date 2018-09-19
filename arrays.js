b336bc18b88033c6eeb250661a038dd8cedc1555
Robert Riley <rob_a_riley@yahoo.com>
2018-09-19 16:54:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element) {
    return (array.unshift (element));
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift (element);
   return array;
 }
