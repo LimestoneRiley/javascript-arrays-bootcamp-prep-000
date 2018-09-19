db69c3faf03212a38045a69ecc610260ef8bde42
Robert Riley <rob_a_riley@yahoo.com>
2018-09-19 16:29:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,4 +32,8 @@ function removeElementFromBeginningOfArray(array){
   return (array.slice(1));
 }
 
-Function 
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return (array);
+}
+
